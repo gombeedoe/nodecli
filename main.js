eca70d283b17998b57a313f1a424806cdf14110f
gombeedoe <gombeedoe@gmail.co>
2020-06-28 18:26:16 +0900

    convert MD to HTML

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const program = require("commander");
 const fs = require("fs");
+// package convert MD to HTML
+const marked = require("marked");
 
 // get file path from CL arg
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
+
+const cliOptions = {
+  gfm: false,
+  ...program.opts(),
+};
 
 // read file async as UTF-8
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
@@ -15,5 +23,8 @@ fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     return;
   }
 
-  console.log(file);
+  const html = marked(file, {
+    gfm: cliOptions.gfm,
+  });
+  console.log(html);
 });
